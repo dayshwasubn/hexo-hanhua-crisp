3c6365f654efb98a1e161496ab3a07add3e7f10f
dayshwasubn <dayshwasubn@gmail.com>
2024-12-18 22:32:47 +0800

    Update PoweredBy.js

--- components/PoweredBy.js
+++ components/PoweredBy.js
@@ -10,9 +10,9 @@ export default function PoweredBy(props) {
       className={`gap-x-1 flex flex-wrap text-sm font-serif ${props.className || ''}`}>
       <span>Powered by</span>
       <a
-        href='https://github.com/tangly1024/NotionNext'
+        href='https://scj-vip.top'
         className='underline justify-start'>
-        NotionNext {siteConfig('VERSION')}
+        狼友收藏夹 {siteConfig('VERSION')}
       </a>
       .
     </div>
