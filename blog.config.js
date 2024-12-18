c090ce945e0b9a5f4925538af5896dd51e2e5b23
dayshwasubn <dayshwasubn@gmail.com>
2024-12-18 23:11:56 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -25,7 +25,7 @@ const BLOG = {
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '狼友收藏夹', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || '欢迎关注！', // 作者简介
+  BIO: process.env.NEXT_PUBLIC_BIO || '狼友您好，请点击页面右下角消息按钮，发送“我要上车”查看本站完整资源获取方式', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://scj-vip.top', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '狼友收藏夹, 91home, 幼幼资源, 小马资源, 91, 91porn, 麻豆, 乱伦, 强奸, 暗网, 海角', // 网站关键词 英文逗号隔开
 
